82676d2a3377a5c8b0579b834a05c076a7aa0ae6
Chengjie-Huang <chuang44@sheffield.ac.uk>
2021-11-16 19:26:34 +0000

    1. 数据库添加字段

--- app/src/main/java/com/example/mapdemo/data/ImageData.kt
+++ app/src/main/java/com/example/mapdemo/data/ImageData.kt
@@ -11,6 +11,9 @@ data class ImageData(
     @PrimaryKey(autoGenerate = true) var id: Int = 0,
     @ColumnInfo(name = "uri") val imageUri: String,
     @ColumnInfo(name = "title") var imageTitle: String,
+    @ColumnInfo(name = "date") var imageDate: String,
+    @ColumnInfo(name = "longitude") var imageLongitude: Float,
+    @ColumnInfo(name = "altitude") var imageAltitude: Float,
     @ColumnInfo(name = "description") var imageDescription: String? = null,
     @ColumnInfo(name = "thumbnailUri") var thumbnailUri: String? = null)
 {
