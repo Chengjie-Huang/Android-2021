a6da063cd47b57055634f29da65bba625d1366d8
Chengjie-Huang <chuang44@sheffield.ac.uk>
2021-12-17 19:02:05 +0000

    update

--- app/src/main/java/com/example/mapdemo/MapsActivity.kt
+++ app/src/main/java/com/example/mapdemo/MapsActivity.kt
@@ -273,7 +273,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMyLocationButton
             }
         }
     }
-
 }
 
 
--- app/src/main/java/com/example/mapdemo/VisitActivity.kt
+++ app/src/main/java/com/example/mapdemo/VisitActivity.kt
@@ -27,7 +27,4 @@ class VisitActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
-
-
 }
